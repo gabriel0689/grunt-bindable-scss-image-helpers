a3878f51a3165f259d44cfcd53685e9c19a24584
jpeg <jmiranda@nextgenins.com>
2018-03-26 11:22:35 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# grunt-bindable-scss-image-helpers
-Modified version of  Daniel Cirmaciu's unmaintained grunt-scss-image-helpers. Creates variables for Compass-like image helpers for url, width and height of images.
+# grunt-scss-image-helpers
+
+Grunt tasks to replace Compass image helpers. It is recommended to run this task with watch task on image folder only.
+
+### Helpers
+
+All helpers have one argument: image name. Name is created relatively to the `imageRoot` option. If we want to use an image in subfolder, we need to enter a path incl. folder name.
+
+Example: `image-url('foo.png')`, `image-url('subfolder/foo.png')`
+
+#### image-width()
+
+Returns width of specified image in pixels (for example `30px`).
+
+#### image-height()
+
+Returns height of specified image in pixels (for example `30px`).
+
+#### image-url()
+
+Returns path to specified image including `url()`.
+
+Example: `background-image: image-url('foo.bar')` is compiled to `background-image: url('foo.bar')`.
+
+When `relativePath` is set to `../img/`, helper will return `url('../img/foo.bar')`. 
+
+When `antiCache` is set to `true`, helper will return path with timestamp: `url('foo.12345678.bar)` or `url('../img/foo.12345678.bar')`.
+
+#### inline-image()
+
+Returns image as data url.
+
+Example: `inline-image('foo.png')` becomes `url('data:image/png;base64,...')`.
+
+### Options
+
+#### imageRoot
+
+Type: `String`  
+Default: `''`
+
+Path to image folder. All image names are created relatively to this folder.
+
+#### prefix
+
+Type: `String`  
+Default: `grunt-images`
+
+Prefix for SCSS variables. These variables are created: `$grunt-images-names`, `$grunt-images-widths`, `$grunt-images-heights`, `$grunt-images-base64` and `$grunt-images-relative-path`.
+
+#### antiCache
+
+Type: `Boolean`  
+Default: `false`
+
+If set to `true`, task generates file names with timestamps to invalidate browser cache.
+
+
+#### relativePath
+
+Type: `String`  
+Default: `''`
+
+All image paths in css are prefixed with this path. For example path `../img/` and image `foo.png` become `../img/foo.png`.
+
+#### size
+
+Type: `Integer`  
+Default: `10240`
+
+Maximum file size for base64 encoding. Images bigger than that won't be encoded.
+
+### Example config
+
+```javascript
+grunt.initConfig({
+    scss_images: {
+        dist: {
+            options: {
+                imageRoot: 'assets/img/',
+                prefix: 'grunt-images',
+                antiCache: true,
+                relativePath: '../img/',
+                size: 4096
+            },
+            files: {
+                'assets/scss/_images.scss': ['assets/img/*.png']
+            }
+        }
+    }
+});
+
+grunt.loadNpmTasks('grunt-scss-image-helpers');
+grunt.registerTask('default', ['scss_images']);
+```
+## License
+
+MIT License • © [Daniel Cirmaciu](http://cirmaciu.cz)
---
+++ tasks/helpers.js
@@ -0,0 +1,142 @@
+'use strict';
+var sizeOf = require('image-size'),
+    path = require('path'),
+    fs = require('fs'),
+    mime = require('mime');
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('scss_images', 'Create image size variables', function() {
+        var options = this.options({
+            prefix: 'grunt-images',
+            imageRoot: '',
+            relativePath: '',
+            antiCache: false,
+            size: 10240
+        });
+
+        this.files.forEach(function(filePair) {
+            var dest = filePair.dest,
+                src = filePair.src,
+                output = '',
+                list = {
+                    ids: [],
+                    widths: [],
+                    heights: [],
+                    paths: [],
+                    base64: []
+                };
+
+            src.forEach(function(file) {
+                var filePath = unixifyPath(path.relative(options.imageRoot, file)),
+                    size = null,
+                    time = 0,
+                    stats = null;
+
+                if (path.extname(file) === '.svg') { // waiting for image-size to implement svg support
+                    size = {
+                        width: 0,
+                        height: 0
+                    };
+                } else {
+                    size = sizeOf(file);
+                }
+
+                list.ids.push(filePath);
+                list.widths.push(size.width);
+                list.heights.push(size.height);
+
+                stats = fs.statSync(file);
+                if (stats.size < options.size) {
+                    list.base64.push('data:' + mime.lookup(file) + ';base64,' + fs.readFileSync(file, 'base64'));
+                } else {
+                    list.base64.push('');
+                }
+
+                if (options.antiCache) {
+                    time = (new Date(stats.mtime).getTime()/1000).toFixed(0);
+                    filePath = filePath.replace(/([^\.]*)$/, time + '.$1');
+                }
+                list.paths.push(filePath);
+            });
+
+            output += '$' + options.prefix + '-ids: ' +  '\'' + list.ids.join('\', \'') + '\';\n';
+            output += '$' + options.prefix + '-names: ' +  '\'' + list.paths.join('\', \'') + '\';\n';
+            
+            if(list.widths != ''){
+             output += '$' + options.prefix + '-widths: ' + list.widths.join(', ') + ';\n';   
+            }
+            else{
+                output += '$' + options.prefix + '-widths:"";\n';
+                console.log("empty list.widths");
+            }
+            if(list.heights != ''){
+             output += '$' + options.prefix + '-heights: ' + list.heights.join(', ') + ';\n';
+            }
+            else{
+                 output += '$' + options.prefix + '-heights: ' ""';\n';
+                 console.log("empty list.heights");
+            }
+
+            
+            output += '$' + options.prefix + '-base64: ' +  '\'' + list.base64.join('\', \'') + '\';\n';
+            output += '$' + options.prefix + '-relative-path: \'' + options.relativePath + '\';\n';
+
+            // this is temporary solution
+            output += [
+                "@function image-width($image) {",
+                    "$index: index($" + options.prefix + "-ids, $image);",
+                    "@if $index {",
+                        "@return nth($" + options.prefix + "-widths, $index)*1px;",
+                    "}",
+                "}",
+
+                "@function image-height($image) {",
+                    "$index: index($" + options.prefix + "-ids, $image);",
+                    "@if $index {",
+                        "@return nth($" + options.prefix + "-heights, $index)*1px;",
+                    "}",
+                "}",
+
+                "@function image-url($image) {",
+                    "$index: index($" + options.prefix + "-ids, $image);",
+                    "@if $index {",
+                        "$image-name: nth($" + options.prefix + "-names, $index);",
+                        "@return url('#{$" + options.prefix + "-relative-path}#{$image-name}')",
+                    "}",
+                    "@else {",
+                        "@return url('#{$" + options.prefix + "-relative-path}#{$image}');",
+                    "}",
+                "}",
+
+                "@function inline-image($image) {",
+                    "$index: index($" + options.prefix + "-ids, $image);",
+                    "@if $index {",
+                        "$base64: nth($" + options.prefix + "-base64, $index);",
+                        "@if $base64 == '' {",
+                            "$image-name: nth($" + options.prefix + "-names, $index);",
+                            "@return url('#{$" + options.prefix + "-relative-path}#{$image-name}')",
+                        "}",
+                        "@else {",
+                            "@return url('#{$base64}')",
+                        "}",
+                    "}",
+                    "@else {",
+                        "@return url('#{$" + options.prefix + "-relative-path}#{$image}');",
+                    "}",
+                "}"
+            ].join('\n');
+
+            grunt.file.write(dest, output);
+
+        });
+
+    });
+
+    var unixifyPath = function(filepath) {
+        if (process.platform === 'win32') {
+            return filepath.replace(/\\/g, '/');
+        } else {
+            return filepath;
+        }
+    };
+};
